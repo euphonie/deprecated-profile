0ae07a350d31515ef0378549414841cc4ff8f413
Joel Cantoral <cs.joel.09@gmail.com>
2022-06-30 14:09:29 -0600

    feat: add firebase config to plug into contact form

---
+++ src/config/firebase.ts
@@ -0,0 +1,31 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(
+    process.env.REACT_APP_FIREBASE_RECAPTCHA_PROVIDER_KEY ?? ''
+  ),
+  isTokenAutoRefreshEnabled: true
+});
+
+export {
+    app,
+    analytics,
+    db,
+    appCheck
+};
