5366faccc5acec8f69a3725146a4591fc5124514
Joel Cantoral <cs.joel.09@gmail.com>
2022-07-02 16:05:31 -0600

    fix(socialbox): fix behance link

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,5 +2,5 @@ export default {
     gh: "//github.com/euphonie",
     linkedin:  "//www.linkedin.com/in/cantoraljoel/",
     hackerrank: "//www.hackerrank.com/cs_joel?hr_r=1",
-    behance: "https://www.behance.net/joelcantoral"
+    behance: "//www.behance.net/joelcantoral"
 };
