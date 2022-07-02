54ba7e6c7b77079d6071210735f48923fef1b57f
Joel Cantoral <cs.joel.09@gmail.com>
2022-07-01 19:13:13 -0600

    feat(mywork): remove skills section

--- src/pages/MyWork.tsx
+++ src/pages/MyWork.tsx
@@ -1,16 +1,12 @@
 import Container from "@mui/material/Container/Container";
 import Grid from "@mui/material/Grid/Grid";
 import ExperienceTimeline from "../components/ExperienceTimeline";
-import Skills from "../components/Skills";
 
 export default () => {
     return(
         <Container disableGutters>
             <Grid container maxWidth="xl">
-                <Grid item xs={12} sm={6}>
-                    <Skills />
-                </Grid>
-                <Grid item xs={12} sm={6}>
+                <Grid item xs={12}>
                    <ExperienceTimeline />
                 </Grid>
             </Grid>
