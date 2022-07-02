0451a397fc8411a4a761748d60cdc96e6b05f5ca
Joel Cantoral <cs.joel.09@gmail.com>
2022-07-02 17:27:58 -0600

    feat(experience): change layout at projects section

--- src/components/experience/ProjectsBox.tsx
+++ src/components/experience/ProjectsBox.tsx
@@ -1,25 +1,44 @@
 import Box from "@mui/material/Box/Box";
 import Button from "@mui/material/Button/Button";
+import Typography from "@mui/material/Typography/Typography";
+import { useTranslation } from "react-i18next";
 import { JobProject, ProjectBoxProps } from "../../common/types";
+import SplitscreenOutlinedIcon from '@mui/icons-material/SplitscreenOutlined';
 
 
 export const ProjectsBox = (props: ProjectBoxProps) => {
+    const { t } = useTranslation();
+    
     return (
         <Box>
-            {
-                props.job.projects.map((project: JobProject, i: number) => {
-                    return (
-                    <Button
-                        size="large"
-                        variant="outlined"
-                        color="secondary"
-                        sx={{ marginTop: '20px', flex: '50%' }}
-                    >
-                        {project.name}
-                    </Button>
-                    )
-                })
-            }
+            <Typography 
+                variant="h6" 
+                textAlign="left"
+                component="span"
+                sx={{
+                    display: 'flex',
+                    gap: '0.8rem',
+                    alignItems: 'center',
+                }}>
+                <SplitscreenOutlinedIcon />
+                {t('projects.title')}
+            </Typography>
+            <Box sx={{display: 'flex', gap: '1rem', flexWrap: 'wrap'}}>
+                {
+                    props.job.projects.map((project: JobProject, i: number) => {
+                        return (
+                        <Button
+                            size="large"
+                            variant="outlined"
+                            color="primary"
+                            sx={{ padding: '20px', marginTop: '20px', flex: '33%' }}
+                        >
+                            {project.name}
+                        </Button>
+                        )
+                    })
+                }
+            </Box>
         </Box>
     );
 }
